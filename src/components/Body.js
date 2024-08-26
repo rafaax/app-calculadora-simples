28283d260715b69aaa627d4c80058554607a6ccc
raphael <rafavfx1@gmail.com>
2024-08-25 23:33:35 -0300

    condicional = clearButton

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { styles }  from './StyleSheet';
+import { sum, div, sub, mult, clear} from '../functions/calculatorFunctions'
 
 export const Body = function() {
+
+    const [num1, setNum1] = useState(0);
+    const [num2, setNum2] = useState(0);
+    const [result, setResult] = useState(0);
+
+    function sum(){
+        setResult(num1 + num2);
+    }
     
+    function sub(){
+        setResult(num1 - num2);
+    }
+    
+    function mult(){
+        setResult(num1 * num2);
+    }
+    
+    function div(){
+        setResult(num1 / num2);
+    }
+    
+    function clear(){
+        setResult(0);
+        setNum2(0);
+        setNum1(0);
+    }
 
     return (
         <>
@@ -49,17 +75,22 @@ export const Body = function() {
             
             </View>
 
-            <View style={{alignItems: 'center'}}>
-                <TouchableOpacity 
-                style={{
-                    ...styles.buttons,
-                    width: 350
-                }}>
-                
-                <Text style={styles.textButtons} onPress={clear}> Limpar </Text>
+            {num1 != '' && num2 != '' && (
+                <View style={{alignItems: 'center'}}>
+                    <TouchableOpacity 
+                    style={{
+                        ...styles.buttons,
+                        width: 350
+                    }}>
+                    
+                    <Text style={styles.textButtons} onPress={clear}> Limpar </Text>
 
-                </TouchableOpacity>
-            </View>
+                    </TouchableOpacity>
+                </View>
+
+            )}
+
+            
         </>
     )
 }
